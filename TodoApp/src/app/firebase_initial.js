b0738c5716d03416154ca0c61b0dc8ad5626be29
Phan Bien <phanbien9x@gmail.com>
2022-04-26 15:11:02 +0700

    FE: Hide firebase config to env file

--- TodoApp/src/app/firebase_initial.js
+++ TodoApp/src/app/firebase_initial.js
@@ -2,19 +2,19 @@ import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken } from 'firebase/messaging';
 
 const app = initializeApp({
-  apiKey: 'AIzaSyAXGagqzsRUHQG6HnF9NftCYI7yh5taPFI',
-  authDomain: 'todoapi-347209.firebaseapp.com',
-  projectId: 'todoapi-347209',
-  storageBucket: 'todoapi-347209.appspot.com',
-  messagingSenderId: '829556782324',
-  appId: '1:829556782324:web:66d8c879c16308bfed5aa1',
-  measurementId: 'G-39Z3TS31Q4',
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 });
 
 export const messaging = getMessaging(app);
 export const getFCMToken = () => {
   getToken(messaging, {
-    vapidKey: 'BG1z8UxgpwR5DsGFwxzvKpvV6PyX1pfGaf6ed3pw-3psoxvNcYko7LQRLSWU7NdVcG-MYoqzhbfNYIP3Tj0wWUI',
+    vapidKey: process.env.REACT_APP_FCM_PUSH_NOTIFY_KEY,
   })
     .then((currentToken) => {
       if (currentToken) {
